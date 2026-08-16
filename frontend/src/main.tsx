904ce2063fe1195c09ad2be56b6fa670ffdf652f
rohitind2007 <rohitind2007@gmail.com>
2026-08-16 16:34:33 +0530

    chore: add @vercel/speed-insights and configure legacy peer deps

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { injectSpeedInsights } from '@vercel/speed-insights'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
-injectSpeedInsights()
+injectSpeedInsights();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
